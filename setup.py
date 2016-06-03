df6208707e9465954a52e8d1e06a757c05303574
Alexey Strokach <alex.strokach@utoronto.ca>
2016-06-03 19:57:19 -0400

    "Release 0.0.3"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datapkg',
-    version="0.0.2",
+    version="0.0.3",
     author='Alexey Strokach',
     author_email='alex.strokach@utoronto.ca',
     packages=find_packages(),
